6787f324dbe6ffa8dc070fc82a6bd27b832218d2
Bijan Daei <khavaran@gmail.com>
2015-12-21 15:17:58 +1100

    Added Merge Sort algorithm from chapter 4.

---
+++ CormenAlgorithms.Ch4DivideNConquer/CormenAlgorithms.Ch4DivideNConquer.Tests/MergeSortTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CormenAlgorithms.Ch4DivideNConquer.Tests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        [TestMethod]
+        public void Should_Sort()
+        {
+            var A = new int[] { 6, 2, 4, 1, 3, 5 };
+            var sut = new MergeSort();
+
+            sut.Sort(A);
+
+            Assert.AreEqual(1, A[0]);
+            Assert.AreEqual(2, A[1]);
+            Assert.AreEqual(3, A[2]);
+            Assert.AreEqual(4, A[3]);
+            Assert.AreEqual(5, A[4]);
+            Assert.AreEqual(6, A[5]);
+        }
+    }
+}
---
+++ CormenAlgorithms.Ch4DivideNConquer/CormenAlgorithms.Ch4DivideNConquer/MergeSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CormenAlgorithms.Common;
+
+namespace CormenAlgorithms.Ch4DivideNConquer
+{
+    public class MergeSort : ISort
+    {
+        public void Sort(int[] A)
+        {
+            Merge_Sort(A, 0, A.Length - 1);
+        }
+
+        private void Merge_Sort(int[] A, int p, int r)
+        {
+            if(p < r)
+            {
+                int q = (p + r) / 2;
+                Merge_Sort(A, p, q);
+                Merge_Sort(A, q + 1, r);
+                Merge(A, p, q, r);
+            }
+        }
+
+        private void Merge(int[] A, int p, int q, int r)
+        {
+            var merged = new int[r - p + 1];
+            int i, j, k;
+            for(i = p, j = q + 1, k = 0; i <= q && j <= r; k++)
+                merged[k] = A[i] <= A[j] ? A[i++] : A[j++];
+            while (i <= q)
+                merged[k++] = A[i++];
+            while (j <= r)
+                merged[k++] = A[j++];
+            for (i = p, k = 0; i <= r; i++, k++)
+                A[i] = merged[k];
+        }
+    }
+}
